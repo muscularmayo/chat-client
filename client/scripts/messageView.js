e636c1287d64949462eb0cdb63fd18035080453f
Chris Risden <christopherrisden@gmail.com>
2021-06-09 20:42:44 -0700

    Complete displaying messages

--- client/scripts/messageView.js
+++ client/scripts/messageView.js
@@ -1,12 +1,17 @@
 var MessageView = {
 
   render: _.template(`
-      <!--
+
       <div class="chat">
-        <div class="username"></div>
-        <div></div>
+        <div class="username">
+        <%= username %>
+        </div>
+        <div class="text">
+        <%= text %>
+        </div>
+
       </div>
-      -->
+
     `)
 
 };
--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -3,9 +3,27 @@ var MessagesView = {
   $chats: $('#chats'),
 
   initialize: function() {
+    Parse.readAll(function(data) {
+      //in this portion we make it give us messages
+      for (var i = 0; i < data.results.length; i ++) {
+        //console.log(data.results[i]);
+        console.log(data);
+        //$('#chats').append(`<p>${data.results[i].text} by ${data.results[i].username}</p>`);
+        MessagesView.renderMessage(data.results[i]);
+
+      }
+    });
   },
 
-  render: function() {
+  renderMessage: function(message) {
+    //each individual obj
+    console.log(message.username, message.text);
+    // ($('<div>' + message.username + message.text + '</div>')).appendTo($('#chats'))
+
+    message.username = message.username || 'undefined';
+    message.text = message.text || 'undefined';
+    $('#chats').append(MessageView.render(message));
+
   }
 
 };
