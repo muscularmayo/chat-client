e0a4f218419a901a47a6186b4fb65db5ac6ca8db
Chris Risden <christopherrisden@gmail.com>
2022-01-03 12:41:06 -0800

    testing git repository

--- client/scripts/rooms.js
+++ client/scripts/rooms.js
@@ -6,7 +6,7 @@ var Rooms = {
     $('#chats').empty();
     Parse.readAll(function(data) {
       //in this portion we make it give us messages
-      console.log('render room data', data);
+      console.log('render room data: ', data);
 
       for (var i = data.results.length - 1; i >= 0; i --) {
 
